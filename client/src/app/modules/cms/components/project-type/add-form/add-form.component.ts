2fba479ee9373500f12c493828d00029c12aa6cd
Sounish Nath <flock.sinasini@gmail.com>
2021-08-21 14:58:59 +0530

    :construction: Added few bugs to resolve later

---
+++ client/src/app/modules/cms/components/project-type/add-form/add-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-form',
+  template: `
+    <p>
+      add-form works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AddFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
