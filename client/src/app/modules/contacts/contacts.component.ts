1fe298804ba57c44161da4ce309c5669b26d6608
Sounish Nath <flock.sinasini@gmail.com>
2021-10-10 22:26:57 +0530

    contacts buttons fix

--- client/src/app/modules/contacts/contacts.component.ts
+++ client/src/app/modules/contacts/contacts.component.ts
@@ -7,22 +7,21 @@ import { Component, OnInit } from '@angular/core';
       <div
         class="m-auto w-2/3 font-semibold text-gray-800 rounded-lg z-40 shadow-lg text-center p-4 bg-white"
       >
-        <img
-          src="https://avatars.githubusercontent.com/u/40270033?v=4"
-          alt="sounishnath-avatar"
-          class="rounded-full w-24 shadow-xl ring-2 ring-black inset m-auto"
-        />
         <div class="text-center my-3 uppercase">HI, I'M SOUNISH NATH!</div>
 
         <div class="my-3">
-          <div class="flex flex-col space-y-4 justify-center items-center">
-            <button class="style">Github ⚡</button>
-            <button class="style">Facebook ⚡</button>
-            <button class="style">Instagram ⚡</button>
-            <button class="style">LinkedIn ⚡</button>
-            <button class="style"> Devfolio ⚡</button>
-            <button class="style">Hover me ⚡</button>
-            <div>Howdy!</div>
+          <div
+            class="flex flex-col space-y-4 flex-wrap justify-center items-center"
+          >
+            <button
+              class="style hover:bg-gray-100 hover:shadow-xl inset-0"
+              *ngFor="let link of social_links"
+            >
+              <a [href]="link.link" target="blank"> {{ link.for }} ⚡ </a>
+            </button>
+            <div class="text-sm text-blue-700 underline">
+              &star; Howdy! I am so lucky you landed on my portfolio!
+            </div>
           </div>
         </div>
       </div>
@@ -38,6 +37,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.css'],
 })
 export class ContactsComponent implements OnInit {
+  social_links: Array<{
+    for: string;
+    link: string;
+  }> = [
+    { for: 'Github', link: 'https://github.com/sounishnath003' },
+    {
+      for: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/sounish-nath-897b30186',
+    },
+    {
+      for: 'Twitter',
+      link: 'https://twitter.com/sounish1',
+    },
+    {
+      for: 'Instagram',
+      link: 'https://www.instagram.com/sounishnath003/',
+    },
+    {
+      for: 'Facebook',
+      link: 'https://www.facebook.com/sounish.nath',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
