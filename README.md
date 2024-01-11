6f4ee49df6978c1b4758a61e840ce24b8f24fa5a
sounish nath <flock.sinasini@gmail.com>
2024-01-12 01:29:11 +0530

    generated the new frontend angular site

--- README.md
+++
@@ -1,4 +0,0 @@
-# portfolio v2.0
-Building own portfolio V2.0
-
-![Portfolio-Homepage-Image](.github/assets/site-home.png)
--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createMongoDBConnection } from "../database";
-import { kickStartTheServer } from "../server";
-
-async function main() {
-  await createMongoDBConnection();
-  kickStartTheServer();
-}
-
-main().catch((e) => console.error(e));
