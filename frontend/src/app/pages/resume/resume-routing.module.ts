dada1b93b8420da15d2a5ad95f235325339bf949
sounish nath <flock.sinasini@gmail.com>
2024-06-15 07:22:22 +0530

    adding the reusme.component and latexengine

--- frontend/src/app/pages/resume/resume-routing.module.ts
+++ frontend/src/app/pages/resume/resume-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResumeComponent } from './resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ResumeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ResumeRoutingModule { }
+export class ResumeRoutingModule {}
--- frontend/src/app/pages/resume/resume.module.ts
+++ frontend/src/app/pages/resume/resume.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ResumeRoutingModule } from './resume-routing.module';
-
+import { ResumeService } from '../resume.service';
+import { LatexLoaderComponent } from './components/latex-loader/latex-loader.component';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    ResumeRoutingModule
-  ]
+  providers: [ResumeService],
+  imports: [CommonModule, ResumeRoutingModule],
 })
-export class ResumeModule { }
+export class ResumeModule {}
