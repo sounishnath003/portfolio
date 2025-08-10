87bb7fe5fb4b6e2c33bf1d96eb935f4194cc97dc
Sounish Nath <flock.sinasini@gmail.com>
2025-08-10 21:18:02 +0530

    feat: Add Hero section with dynamic attributes and profile bio
    - Implemented Hero.vue component to display a greeting, dynamic job attributes, and a profile bio.
    - Added functionality to cycle through job attributes every 1.5 seconds.
    - Integrated a download link for the resume and a navigation link to the about section.
    - Created WorkAt.vue component to showcase previous work experiences with company logos.
    - Updated Home.vue to include the new Hero component.
    - Added NotFound.vue for handling 404 errors with a user-friendly message.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,27 @@
-
 <template>
   <Header />
-  <Home />
+  <component :is="currentView" />
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue';
 import Header from './components/Header.vue';
-import Home from './pages/Home.vue';
+import Home from './pages/home/Home.vue';
+import NotFound from './pages/not-found/NotFound.vue';
 
+const routes: Record<string, any> = {
+  '/': Home
+};
+
+// Get the current path
+const currentPath = ref(window.location.hash);
+
+// Add event listerner
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 </script>
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -9,21 +9,29 @@
           </a>
         </div>
         <div class="flex lg:hidden">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = true">
             <span class="sr-only">Open main menu</span>
             <Bars3Icon class="size-6" aria-hidden="true" />
           </button>
         </div>
         <div class="hidden lg:flex lg:gap-x-12">
-          <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm/6 font-semibold text-gray-900">{{ item.name }}</a>
+          <a v-for="item in navigation" :key="item.name" :href="item.href"
+            class="text-sm/6 font-semibold text-gray-900 dark:text-white">{{ item.name }}</a>
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+          <a href="#/login" class="text-sm/6 font-semibold text-gray-900 dark:text-white flex items-center gap-1">
+            <div aria-hidden="true">
+              <LockClosedIcon class="w-6" />
+            </div>
+            <div>Log in</div>
+          </a>
         </div>
       </nav>
       <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
         <div class="fixed inset-0 z-50" />
-        <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel
+          class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div class="flex items-center justify-between">
             <a href="#" class="-m-1.5 p-1.5">
               <span class="sr-only">Sounish Nath</span>
@@ -37,10 +45,14 @@
           <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
               <div class="space-y-2 py-6">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+                <a v-for="item in navigation" :key="item.name" :href="item.href"
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{
+                  item.name }}</a>
               </div>
               <div class="py-6">
-                <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
+                <a href="#"
+                  class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log
+                  in</a>
               </div>
             </div>
           </div>
@@ -54,12 +66,14 @@
 import { ref } from 'vue'
 import { Dialog, DialogPanel } from '@headlessui/vue'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { LockClosedIcon } from '@heroicons/vue/24/solid'
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Projects', href: '#/projects' },
+  { name: 'Blogs', href: '#/blogs' },
+  { name: 'About', href: '#/about' },
+  { name: 'Resume', href: '#/resume' },
+  { name: 'Uses', href: '#/uses' },
 ]
 
 const mobileMenuOpen = ref(false)
