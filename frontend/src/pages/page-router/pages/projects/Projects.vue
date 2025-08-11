9290fb13ae411dd61887b4df5a1b7b7994da6174
Sounish Nath <flock.sinasini@gmail.com>
2025-08-12 01:39:59 +0530

    feat: Add introductory text to Projects page for enhanced context

--- frontend/src/pages/page-router/pages/projects/Projects.vue
+++ frontend/src/pages/page-router/pages/projects/Projects.vue
@@ -1,4 +1,8 @@
 <template>
+    <p class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100 hhidden md:block">
+        &bull; Things I’ve made trying to put my dent in the universe.
+    </p>
+
     <ProjectCard v-for="project in Portfolio.projectsPage.projects" :project="project" />
 </template>
 
