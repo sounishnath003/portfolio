8d43886d88580a04e8406c82f1dc4a0df23196d7
Sounish Nath <flock.sinasini@gmail.com>
2025-08-12 12:55:07 +0530

    refactor: Improve code readability and structure across multiple components
    - Enhanced the structure of App.vue, Header.vue, and various page components by consolidating attributes and removing unnecessary line breaks for better readability.
    - Updated image elements and links for consistency and improved accessibility.
    - Adjusted layout and spacing in components like RecommendationCard.vue, ProjectCard.vue, and ExperienceCard.vue for a more cohesive design.
    - Ensured proper formatting in portfolioDatabase.ts for better data management.

--- frontend/src/pages/page-router/pages/uses/Uses.vue
+++ frontend/src/pages/page-router/pages/uses/Uses.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        <p class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100 hhidden md:block">
-            &bull; Software I use, gadgets I love, and other things I recommend.
-        </p>
-    </div>
-
-
+  <div>
+    <p
+      class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100 hhidden md:block"
+    >
+      &bull; Software I use, gadgets I love, and other things I recommend.
+    </p>
+  </div>
 </template>
